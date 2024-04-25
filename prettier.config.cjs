35086d3cb64cd9b5f2b8545a302d0bcd3615a50c
ansh399kakkar <anshkakkar399@gmail.com>
2024-04-25 12:08:15 +0530

    first

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+import prettierPluginTailwindCSS from "prettier-plugin-tailwindcss";
+
+const config = {
+  plugins: [prettierPluginTailwindCSS],
+};
+
+export default config;
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+
+
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
